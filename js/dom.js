ad1e414bb9b87756d65d11487219d142638c7335
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-16 04:17:52 +0600

    first commit

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+console.log('Hello from Js');
+
+// const test= document.getElementById('test');
+// // test.setAttribute('class','text-red');
+// // test.innerHTML=
+// // `
+// //  <h1>New Content </h1>
+// // `
+// // test.innerText='Section -1';
+// const child= test.childNodes;
+// console.log(child.length);
+
+let section1 = document.getElementById('check');
+let pNode=section1.S;
+console.log(pNode);
+
+
+
+
+
+
+
+
